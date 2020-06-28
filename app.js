62de3a57ea02416e8ffbb50264d7f719cb0bc5ea
Hyoniii <phhphh841@hanmail.net>
2020-06-28 11:00:58 +0900

    aa

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -37,5 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-//dksdkdk
+
 module.exports = app;
